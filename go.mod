22b5b1d22b417c9e1ff31bf91b4b747f53c8fb7a
KusakabeSi <si@kskb.eu.org>
2021-08-23 13:23:04 +0000

    use orderd map

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/KusakabeSi/go-cache
 
 go 1.16
+
+require github.com/wk8/go-ordered-map v0.2.0 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package fixed_time_cache
 
 import (
-	"container/list"
 	"fmt"
 	"sync"
 	"time"
+
+	orderedmap "github.com/wk8/go-ordered-map"
 )
 
 type Item struct {
@@ -12,23 +13,21 @@ type Item struct {
 	Expiration time.Time
 }
 
-type timeout struct {
-	Key    interface{}
-	Expire time.Time
-}
-
 type Cache struct {
 	expiration    time.Duration
+	ExtendOnGet   bool
 	items         sync.Map
-	timeouts      *list.List
+	timeouts      *orderedmap.OrderedMap
+	timeouts_lock sync.RWMutex
 	nextClear     time.Time
 	ClearCooldown time.Duration
 }
 
-func NewCache(defaultExpiration time.Duration, clearcooldown time.Duration) *Cache {
+func NewCache(defaultExpiration time.Duration, extendOnGet bool, clearcooldown time.Duration) *Cache {
 	return &Cache{
 		expiration:    defaultExpiration,
-		timeouts:      list.New(),
+		ExtendOnGet:   extendOnGet,
+		timeouts:      orderedmap.New(),
 		ClearCooldown: clearcooldown,
 	}
 }
@@ -37,12 +36,15 @@ func (c *Cache) Set(key interface{}, val interface{}) {
 	c.ClearExpired()
 	exptime := time.Now().Add(c.expiration)
 	c.items.Store(key, Item{Object: val, Expiration: exptime})
-	c.timeouts.PushBack(timeout{Key: key, Expire: exptime})
+	c.ExtendExpire(key, exptime)
 }
 
 func (c *Cache) Get(key interface{}) (val interface{}, ok bool) {
 	if val, ok := c.items.Load(key); ok {
 		if val.(Item).Expiration.After(time.Now()) {
+			if c.ExtendOnGet {
+				c.ExtendExpire(key, time.Now().Add(c.expiration))
+			}
 			return val.(Item).Object, true
 		} else {
 			c.items.Delete(key)
@@ -51,28 +53,35 @@ func (c *Cache) Get(key interface{}) (val interface{}, ok bool) {
 	return nil, false
 }
 
+func (c *Cache) ExtendExpire(key interface{}, exptime time.Time) {
+	c.timeouts_lock.Lock() //mode this item to the end of the linked list
+	c.timeouts.Delete(key)
+	c.timeouts.Set(key, exptime)
+	c.timeouts_lock.Unlock()
+}
+
 func (c *Cache) ClearExpired() {
 	if c.ClearCooldown != 0 && time.Now().Before(c.nextClear) {
 		return
 	}
 	c.nextClear = time.Now().Add(c.ClearCooldown)
-	for c.timeouts.Len() > 0 {
-		first := c.timeouts.Front()
-		switch first_elem := first.Value.(type) {
-		case timeout:
-			if time.Now().After(first_elem.Expire) {
-				c.items.Delete(first_elem.Key)
-				c.timeouts.Remove(first)
-			} else {
-				return
-			}
+	c.timeouts_lock.RLock()
+	for pair := c.timeouts.Oldest(); pair != nil; pair = pair.Next() {
+		if time.Now().After(pair.Value.(time.Time)) {
+			c.timeouts_lock.RUnlock()
+			c.timeouts_lock.Lock()
+			c.timeouts.Delete(pair.Key)
+			c.items.Delete(pair.Key)
+			c.timeouts_lock.Unlock()
+			c.timeouts_lock.RLock()
 		}
 	}
+	c.timeouts_lock.RUnlock()
 }
 
 func example() {
 
-	c := NewCache(3*time.Second, 1*time.Second)
+	c := NewCache(3*time.Second, false, 1*time.Second)
 	c.Set(5, "Hello")
 	aaa, ok := c.Get(5)
 	switch a := aaa.(type) {
