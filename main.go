8ee1215c700a57574440c50fa626b1012486d369
Kusakabe Si <si@kskb.eu.org>
2021-12-10 16:45:31 +0000

    Add check for expire <= 0

--- main.go
+++ main.go
@@ -33,6 +33,9 @@ func NewCache(defaultExpiration time.Duration, extendOnGet bool, clearcooldown t
 }
 
 func (c *Cache) Set(key interface{}, val interface{}) {
+	if c.expiration <= 0 {
+		return
+	}
 	c.ClearExpired()
 	exptime := time.Now().Add(c.expiration)
 	c.items.Store(key, Item{Object: val, Expiration: exptime})
@@ -40,6 +43,9 @@ func (c *Cache) Set(key interface{}, val interface{}) {
 }
 
 func (c *Cache) Get(key interface{}) (val interface{}, ok bool) {
+	if c.expiration <= 0 {
+		return nil, false
+	}
 	if val, ok := c.items.Load(key); ok {
 		if val.(Item).Expiration.After(time.Now()) {
 			if c.ExtendOnGet {
@@ -54,6 +60,9 @@ func (c *Cache) Get(key interface{}) (val interface{}, ok bool) {
 }
 
 func (c *Cache) ExtendExpire(key interface{}, exptime time.Time) {
+	if c.expiration <= 0 {
+		return
+	}
 	c.timeouts_lock.Lock() //mode this item to the end of the linked list
 	c.timeouts.Delete(key)
 	c.timeouts.Set(key, exptime)
@@ -61,6 +70,9 @@ func (c *Cache) ExtendExpire(key interface{}, exptime time.Time) {
 }
 
 func (c *Cache) ClearExpired() {
+	if c.expiration <= 0 {
+		return
+	}
 	if c.ClearCooldown != 0 && time.Now().Before(c.nextClear) {
 		return
 	}
